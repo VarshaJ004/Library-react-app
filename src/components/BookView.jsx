ebeed8f9139cb836e0de3c4d97d7d9f5185a4ba7
VarshaJ004 <varshajohnson333@gmail.com>
2026-07-10 10:56:09 +0530

    JSON Formatted

--- src/components/BookView.jsx
+++ src/components/BookView.jsx
@@ -1,144 +1,147 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navigationbar from './Navigationbar'
 import './Book.css'
 
 const BookView = () => {
-  return (
-    <div>
-        <Navigationbar />
-        <div className="container">
-        <h1 className='page-heading mt-4'><center>HAPPY READ!!</center></h1>
-            <div className="row">
-                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
-                    <div className="row g-3">
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-                            <div class="card">
-                                <div class="card-body">
-                                    <img src="https://images.unsplash.com/photo-1544947950-fa07a98d237f?w=500&q=80" class="card-img-top" alt="..." height="400"/>
-                                    <h5 class="card-title">The Midnight Library</h5>
-                                    <p class="card-text">Author: Matt Haig</p>
-                                    <p class="card-text"><strong>Price: ₹399</strong></p>
-                                    <a href="#" class="btn btn-primary">Add to Cart</a>
+    const [data, changeData] = useState([
+        {
+            title: "The Midnight Library",
+            image: "https://images.unsplash.com/photo-1544947950-fa07a98d237f?w=500&q=80",
+            author: "Matt Haig",
+            price: 399
+        },
+        {
+            title: "Atomic Habits",
+            image: "https://images.unsplash.com/photo-1543002588-bfa74002ed7e?w=500&q=80",
+            author: "James Clear",
+            price: 549
+        },
+        {
+            title: "To Kill a Mockingbird",
+            image: "https://images.unsplash.com/photo-1618666012174-83b441c0bc76?w=500&q=80",
+            author: "Harper Lee",
+            price: 299
+        },
+        {
+            title: "The Alchemist",
+            image: "https://images.unsplash.com/photo-1495640388908-05fa85288e61?w=500&q=80",
+            author: "Paulo Coelho",
+            price: 320
+        },
+        {
+            title: "Educated",
+            image: "https://images.unsplash.com/photo-1512820790803-83ca734da794?w=500&q=80",
+            author: "Tara Westover",
+            price: 450
+        },
+        {
+            title: "Where the Crawdads Sing",
+            image: "https://images.unsplash.com/photo-1629992101753-56d196c8aabb?w=500&q=80",
+            author: "Delia Owens",
+            price: 425
+        },
+        {
+            title: "Sapiens",
+            image: "https://images.unsplash.com/photo-1532012197267-da84d127e765?w=500&q=80",
+            author: "Yuval Noah Harari",
+            price: 599
+        },
+        {
+            title: "Thinking, Fast and Slow",
+            image: "https://images.unsplash.com/photo-1516979187457-637abb4f9353?w=500&q=80",
+            author: "Daniel Kahneman",
+            price: 499
+        },
+        {
+            title: "The Silent Patient",
+            image: "https://images.unsplash.com/photo-1541963463532-d68292c34b19?w=500&q=80",
+            author: "Alex Michaelides",
+            price: 350
+        },
+        {
+            title: "Project Hail Mary",
+            image: "https://images.unsplash.com/photo-1476275466078-4007374efbbe?w=500&q=80",
+            author: "Andy Weir",
+            price: 580
+        }
+    ])
+
+
+    return (
+        <div>
+
+            <Navigationbar />
+
+            <div className="container">
+
+                <h1 className='page-heading mt-4 text-center'>
+                    HAPPY READ!!
+                </h1>
+
+
+                <div className="row mt-4">
+
+                    {
+                        data.map((value, index) => {
+
+                            return (
+
+                                <div 
+                                className="col col-12 col-sm-6 col-md-4 col-lg-3 mb-4"
+                                key={index}
+                                >
+
+                                    <div className="card shadow h-100">
+
+                                        <div className="card-body">
+
+                                            <img 
+                                            src={value.image}
+                                            className="card-img-top book-img"
+                                            alt={value.title}
+                                            />
+
+
+                                            <h5 className="card-title mt-3">
+                                                {value.title}
+                                            </h5>
+
+
+                                            <p className="card-text">
+                                                Author: {value.author}
+                                            </p>
+
+
+                                            <p className="card-text">
+                                                <strong>
+                                                    Price: ₹{value.price}
+                                                </strong>
+                                            </p>
+
+
+                                            <button className="btn btn-primary w-100">
+                                                Add to Cart
+                                            </button>
+
+
+                                        </div>
+
+                                    </div>
+
                                 </div>
-                            </div>
-                        </div>
 
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-                            <div class="card">
-                                <div class="card-body">
-                                    <img src="https://images.unsplash.com/photo-1543002588-bfa74002ed7e?w=500&q=80" class="card-img-top" alt="..." height="400"/>
-                                    <h5 class="card-title">Atomic Habits</h5>
-                                    <p class="card-text">Author: James Clear</p>
-                                    <p class="card-text"><strong>Price: ₹549</strong></p>
-                                    <a href="#" class="btn btn-primary">Add to Cart</a>
-                                </div>
-                            </div>
-                        </div>
+                            )
 
-                        <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
-                            <div class="card">
-                                <div class="card-body">
-                                    <img src="https://images.unsplash.com/photo-1618666012174-83b441c0bc76?w=500&q=80" class="card-img-top" alt="..." height="400"/>
-                                    <h5 class="card-title">To Kill a Mockingbird</h5>
-                                    <p class="card-text">Author: Harper Lee</p>
-                                    <p class="card-text"><strong>Price: ₹299</strong></p>
-                                    <a href="#" class="btn btn-primary">Add to Cart</a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-                            <div class="card">
-                                <div class="card-body">
-                                    <img src="https://images.unsplash.com/photo-1495640388908-05fa85288e61?w=500&q=80" class="card-img-top" alt="..." height="400"/>
-                                    <h5 class="card-title">The Alchemist</h5>
-                                    <p class="card-text">Author: Paulo Coelho</p>
-                                    <p class="card-text"><strong>Price: ₹320</strong></p>
-                                    <a href="#" class="btn btn-primary">Add to Cart</a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-                            <div class="card">
-                                <div class="card-body">
-                                    <img src="https://images.unsplash.com/photo-1512820790803-83ca734da794?w=500&q=80" class="card-img-top" alt="..." height="400"/>
-                                    <h5 class="card-title">Educated</h5>
-                                    <p class="card-text">Author: Tara Westover</p>
-                                    <p class="card-text"><strong>Price: ₹450</strong></p>
-                                    <a href="#" class="btn btn-primary">Add to Cart</a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
-                            <div class="card">
-                                <div class="card-body">
-                                    <img src="https://images.unsplash.com/photo-1629992101753-56d196c8aabb?w=500&q=80" class="card-img-top" alt="..." height="400"/>
-                                    <h5 class="card-title">Where the Crawdads Sing</h5>
-                                    <p class="card-text">Author: Delia Owens</p>
-                                    <p class="card-text"><strong>Price: ₹425</strong></p>
-                                    <a href="#" class="btn btn-primary">Add to Cart</a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-                            <div class="card">
-                                <div class="card-body">
-                                    <img src="https://images.unsplash.com/photo-1532012197267-da84d127e765?w=500&q=80" class="card-img-top" alt="..." height="400"/>
-                                    <h5 class="card-title">Sapiens</h5>
-                                    <p class="card-text">Author: Yuval Noah Harari</p>
-                                    <p class="card-text"><strong>Price: ₹599</strong></p>
-                                    <a href="#" class="btn btn-primary">Add to Cart</a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-                            <div class="card">
-                                <div class="card-body">
-                                    <img src="https://images.unsplash.com/photo-1516979187457-637abb4f9353?w=500&q=80" class="card-img-top" alt="..." height="400"/>
-                                    <h5 class="card-title">Thinking, Fast and Slow</h5>
-                                    <p class="card-text">Author: Daniel Kahneman</p>
-                                    <p class="card-text"><strong>Price: ₹499</strong></p>
-                                    <a href="#" class="btn btn-primary">Add to Cart</a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                <div class="card-body">
-                                    <img src="https://images.unsplash.com/photo-1541963463532-d68292c34b19?w=500&q=80" class="card-img-top" alt="..." height="400"/>
-                                    <h5 class="card-title">The Silent Patient</h5>
-                                    <p class="card-text">Author: Alex Michaelides</p>
-                                    <p class="card-text"><strong>Price: ₹350</strong></p>
-                                    <a href="#" class="btn btn-primary">Add to Cart</a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-                            <div class="card">
-                                <div class="card-body">
-                                    <img src="https://images.unsplash.com/photo-1476275466078-4007374efbbe?w=500&q=80" class="card-img-top" alt="..." height="400"/>
-                                    <h5 class="card-title">Project Hail Mary</h5>
-                                    <p class="card-text">Author: Andy Weir</p>
-                                    <p class="card-text"><strong>Price: ₹580</strong></p>
-                                    <a href="#" class="btn btn-primary">Add to Cart</a>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
+                        })
+                    }
 
                 </div>
+
             </div>
+
         </div>
-    </div>
-  )
+    )
 }
 
 export default BookView
